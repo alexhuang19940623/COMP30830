86012ec01f516298280067ded4fc8931054a44f1
Tianyu Huang <tianyu.huang@ucdconnect.ie>
2022-01-25 10:31:37 +0000

    practical1-test

--- practical1.py
+++ practical1.py
@@ -1,38 +1,12 @@
-import os
-import commands
-import string
-import socket
-import json
-def base_info():
-    """
-    :return HOSTNAME, IPADDR, CPU, CPUIDLE, MEM, AVAI_MEM, VERSION(6/7), MACHINE(64/32)
-    """
-    ifname='eth0'
-    osinfo = os.uname()
-    HOSTNAME = osinfo[1]
-    VERSION  = osinfo[2].split('.')[-2]
-    MACHINE  = osinfo[-1]
-    mem_cmd = "awk '/MemTotal/{printf(\"%0.2f\",$2/1000/1000)}' /proc/meminfo"
-    MEM = int(round(float(commands.getoutput(mem_cmd))))
+def showinfo(tip, info):
+    print("{}:{}".format(tip,info))
  
-    cpu_cmd = "awk '/processor/{cpu+=1}END{print cpu}' /proc/cpuinfo"
-    CPU = commands.getoutput(cpu_cmd)
-    if VERSION == 'el7':
-        avai_mem_cmd = "awk '/MemAvailable/ {printf(\"%0.2f\",$2/1000/1000)}' /proc/meminfo"
-        AVAI_MEM =  commands.getoutput(avai_mem_cmd)
-        cpuidle_cmd = "top -bcn 1 | head |awk '/Cpu/ {print $8}'"
-        CPUIDLE = commands.getoutput(cpuidle_cmd)
-    if VERSION == 'el6':
-        ##centos6 可用内存计算方法:MemFree+Buffers+Cached
-        avai_mem_cmd = "sed -n '2,4p' /proc/meminfo |awk '{sum+=$2}END{printf(\"%.2f\",sum/1000/1000)}'"
-        AVAI_MEM =  commands.getoutput(avai_mem_cmd)
-        cpuidle_cmd = "top -bcn 1 | sed -n 's/ //g;3p' | awk -F\"%|,\" '{print $7}'"
-        CPUIDLE = commands.getoutput(cpuidle_cmd)
-    unix_socker = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    IPADDR = socket.inet_ntoa(fcntl.ioctl(
-        unix_socker.fileno(),
-        0x8915,  # SIOCGIFADDR
-        struct.pack('256s', ifname[:15])
-    )[20:24])
  
-    return HOSTNAME, IPADDR, CPU, CPUIDLE, MEM, AVAI_MEM, VERSION, MACHINE
+showinfo("操作系统及版本信息",platform.platform())
+showinfo('获取系统版本号',platform.version())
+showinfo('获取系统名称', platform.system())
+showinfo('系统位数', platform.architecture())
+showinfo('计算机类型', platform.machine())
+showinfo('计算机名称', platform.node())
+showinfo('处理器类型', platform.processor())
+showinfo('计算机相关信息', platform.uname())
