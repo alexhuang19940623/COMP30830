971a5cd220c71c36da312147297ae7c0bbe91323
Tianyu Huang <tianyu.huang@ucdconnect.ie>
2022-01-25 11:12:34 +0000

    setup

--- setup.py
+++ setup.py
@@ -2,6 +2,27 @@ import platform
 import socket
 import multiprocessing
 import psutil
+from setuptools import setup, find_packages
+ 
+setup(
+    name='pip_setup',
+    version='0.1.0',
+    description='just for test',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+    ],
+    author='bin381',
+    url='https://github.com',
+    author_email='',
+    license='MIT',
+    packages=find_packages(),#需要处理哪里packages，当然也可以手动填，例如['pip_setup', 'pip_setup.ext']
+    include_package_data=False,
+    zip_safe=True,
+)
+
 def showinfo(tip, info):
     print("{}:{}".format(tip,info))
 
