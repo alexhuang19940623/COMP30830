576ce604d268584b06d005726eb3feb2d96ddc46
Tianyu Huang <tianyu.huang@ucdconnect.ie>
2022-01-27 10:44:48 +0000

    test

--- setup.py
+++ setup.py
@@ -1,22 +1,16 @@
-import platform
-import socket
-import multiprocessing
-import psutil
 
-def showinfo(tip, info):
-    print("{}:{}".format(tip,info))
-
-
-showinfo('Machine name', platform.node())
-showinfo('OS version',platform.version())
-showinfo('OS name', platform.system())
-showinfo('OS bit', platform.architecture())
-showinfo('CPU version', platform.processor())
-showinfo('CPU numbers', multiprocessing.cpu_count())
-showinfo('Amount of memories', psutil.virtual_memory().total)
-showinfo('IP Address', socket.gethostbyname(socket.gethostname()))
-
-
-
-
-
+from setuptools import setup, find_packages
+ 
+setup(
+    name="practical 2",
+    version="1.0",
+    license="MIT Licence",
+ 
+    url="practical 2\__init__.py",
+    author="hezd",
+    author_email="w-s-h-z-d@163.com",
+ 
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    install_requires=[])
